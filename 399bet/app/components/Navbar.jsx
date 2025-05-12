acec107a069488f642ac5264c286e4eaf75c4929
zeeshan00013 <za953042@gmail.com>
2025-05-12 10:41:26 +0500

    navbar

--- 399bet/app/components/Navbar.jsx
+++ 399bet/app/components/Navbar.jsx
@@ -1,79 +1,30 @@
-"use client";
+import { LogInIcon } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <header className="bg-[#067831] sticky top-0 z-50 py-6">
-      <nav className="container mx-auto px-4 md:px-10 flex items-center justify-between">
-        <div className="text-xl font-bold text-[#f0c059c5]">
-          <Link href="/">
-            <p className="text-2xl md:text-5xl flex items-center font-extrabold text-white">
-              399<span className="text-[#ffed22]">Bet</span>.com
-            </p>
-          </Link>
+    <header className="bg-[#067831] sticky top-0 z-50 py-6 border-b-2 border-[#04953be6]">
+      <nav className=" px-4 md:px-14  w-full">
+        <div className="sm:flex items-center  sm:justify-between w-full">
+          <div className="text-xl font-bold text-[#f0c059c5] ">
+            <Link href="/">
+              <p className="text-2xl md:text-5xl flex items-center font-extrabold text-white">
+                399<span className="text-[#ffed22]">Bet</span>.com
+              </p>
+            </Link>
+          </div>
+          <div className="">
+            <button>
+              <a
+                href="https://www.399bet.net/?id=705322021&currency=PKR&type=4"
+                className="flex items-center justify-center bg-[#ffffff] hover:bg-[#e1e3e2] text-[#067831] text-lg font-semibold px-4 sm:px-10 py-3 rounded-md shadow-md transition mt-5 gap-2 "
+              >
+                Register <LogInIcon />
+              </a>
+            </button>
+          </div>
         </div>
-
-        <ul className="hidden md:flex space-x-6 text-white font-medium">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/">About</Link>
-          </li>
-
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-        </ul>
-
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="md:hidden text-gray-100 focus:outline-none"
-          aria-label="Toggle menu"
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            viewBox="0 0 24 24"
-          >
-            {isOpen ? (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            ) : (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            )}
-          </svg>
-        </button>
       </nav>
-
-      {isOpen && (
-        <div className="md:hidden px-4 pb-4  border-t">
-          <ul className="space-y-3 text-white font-medium">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/">About</Link>
-            </li>
-
-            <li>
-              <Link href="/">Contact</Link>
-            </li>
-          </ul>
-        </div>
-      )}
     </header>
   );
 }
